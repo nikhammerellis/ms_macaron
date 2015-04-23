5e4c803699bd33f9ee144d0ae6348ea4544c7b7f
Nik Hammer-Ellis <nikhammerellis@gmail.com>
2015-04-23 13:15:05 -0700

    mostly finished

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,10 +21,10 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: :plain,
+    authentication: :login,
     enable_starttls_auto: true,
-    user_name: "nikhammerellis@gmail.com",
-    password: "assmaster",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
     openssl_verify_mode: "none"
   }
 
